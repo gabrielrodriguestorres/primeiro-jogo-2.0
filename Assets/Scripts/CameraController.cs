5f406b468c3298930abacc9a839ff3f25e494e9c
nerftwitch <gabrielrodrigues.rt@gmail.com>
2021-06-13 21:46:36 -0300

    mudei pra menos

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int a1, a2;
+
+        a1 = 32;
+        a2 = 35;
+        
+        Debug.Log(a2 - a1);
+
+    }
+}
