5917e46a4a3c8228ec6a3a73695f14e97fd79f67
nerftwitch <gabrielrodrigues.rt@gmail.com>
2021-06-15 21:03:40 -0300

    asqwe12

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class playerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
